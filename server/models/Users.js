57cb0d18242c7273ba882d62adde0a84a095e903
asalas <67822688+andrewjsalas@users.noreply.github.com>
2023-08-20 15:36:40 -0700

    Formatting
    Model formatting.

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    username: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    role: { 
+        type: String, 
+        enum: ['user', 'admin'], 
+        default: 'user' 
+    },
 });
 
 const User = mongoose.model('User', userSchema);
